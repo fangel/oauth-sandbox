c1abe541a48b3165a83846a577662340ccbc9aec
fangel <fangel@sevengoslings.net>
2009-11-14 11:24:57 -0800

    Added sanitizing to the OAuthUtil::getHeaders function with apache_request_header exists

--- library/OAuth.php
+++ library/OAuth.php
@@ -765,24 +765,40 @@ class OAuthUtil {
     if (function_exists('apache_request_headers')) {
       // we need this to get the actual Authorization: header
       // because apache tends to tell us it doesn't exist
-      return apache_request_headers();
-    }
-    // otherwise we don't have apache and are just going to have to hope
-    // that $_SERVER actually contains what we need
-    $out = array();
-    foreach ($_SERVER as $key => $value) {
-      if (substr($key, 0, 5) == "HTTP_") {
-        // this is chaos, basically it is just there to capitalize the first
-        // letter of every word that is not an initial HTTP and strip HTTP
-        // code from przemek
+      $headers = apache_request_headers();
+
+      // sanitize the output of apache_request_headers because
+      // we always want the keys to be Cased-Like-This and arh()
+      // returns the headers in the same case as they are in the
+      // request
+      $out = array();
+      foreach( $headers AS $key => $value ) {
         $key = str_replace(
-          " ",
-          "-",
-          ucwords(strtolower(str_replace("_", " ", substr($key, 5))))
-        );
+            " ",
+            "-",
+            ucwords(strtolower(str_replace("-", " ", $key)))
+          );
         $out[$key] = $value;
       }
+    } else {
+      // otherwise we don't have apache and are just going to have to hope
+      // that $_SERVER actually contains what we need
+      $out = array();
+      foreach ($_SERVER as $key => $value) {
+        if (substr($key, 0, 5) == "HTTP_") {
+          // this is chaos, basically it is just there to capitalize the first
+          // letter of every word that is not an initial HTTP and strip HTTP
+          // code from przemek
+          $key = str_replace(
+            " ",
+            "-",
+            ucwords(strtolower(str_replace("_", " ", substr($key, 5))))
+          );
+          $out[$key] = $value;
+        }
+      }
     }
+
     return $out;
   }
 
