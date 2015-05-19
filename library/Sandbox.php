1779a13dba548ce3f22060eefe53338180919747
Morten Fangel <fangel@sevengoslings.net>
2015-05-19 09:12:01 +0200

    Added CORS headers to all pages
    CORS headers to allow GET, POST and OPTIONS requests from all origins have been added to all pages, to enable JS-clients to test against the sandbox.

--- library/Sandbox.php
+++ library/Sandbox.php
@@ -50,3 +50,5 @@ if( isset($_SESSION['user']) ) {
 }
 
 header("Content-Type: text/html; charset=UTF-8");
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
